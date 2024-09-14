9775b5f18f4602e1d2327f60b41ca4aa2868672e
ms-jahan <contact@sabit.dev>
2024-09-14 06:53:45 +0600

    added model, migration and routes

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('admin/entries')->group(function () {
+    Route::get('/', [EntryController::class, 'index'])->name('entries.index'); 
+    Route::get('/create', [EntryController::class, 'create'])->name('entries.create');
+    Route::post('/', [EntryController::class, 'store'])->name('entries.store');
+    Route::get('/{entry}/edit', [EntryController::class, 'edit'])->name('entries.edit');
+    Route::put('/{entry}', [EntryController::class, 'update'])->name('entries.update');
+    Route::delete('/{entry}', [EntryController::class, 'destroy'])->name('entries.destroy');
+});
